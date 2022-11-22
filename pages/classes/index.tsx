327cc87e38e8d547a7cf828d6b8081154c14612f
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-11-22 17:19:42 -0300

    feat(Classes): create page

---
+++ pages/classes/index.tsx
@@ -0,0 +1,57 @@
+import { NextPage } from "next";
+import React from "react";
+import { useSelector } from "react-redux";
+import { Input } from "antd";
+import 'antd/dist/antd.css';
+import styles from "@/styles/Classes.module.css";
+import { Header } from "../../src/components/header";
+import { FilterButton } from "../../src/components/filterButton";
+
+const Classes: NextPage = () => {
+  const { enums } = useSelector((state: any) => state);
+  const hasEnums = Object.keys(enums).length;
+
+  const { Search } = Input;
+
+  const handleOnSearch = () => {};
+
+  return hasEnums ? (
+    <div className={styles.container}>
+      <Header />
+
+      <div className={styles.body}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Turmas</h1>
+
+          <button className={styles.button}>Criar</button>
+        </div>
+
+        <div className={styles.filterContainer}>
+          <div className={styles.filterLeftSide}>
+            <FilterButton title="Disciplina" />
+
+            <FilterButton title="Semestre" />
+          </div>
+
+          <div className={styles.filterRightSide}>
+            <div className={styles.inputSearchDiv}>
+              <label className={styles.inputSearchName}>Nome:</label>
+              <Search
+                placeholder="Digite um nome..."
+                allowClear
+                onSearch={handleOnSearch}
+                style={{ width: 300 }}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div>
+          
+        </div>
+      </div>
+    </div>
+  ) : null;
+};
+
+export default Classes;
