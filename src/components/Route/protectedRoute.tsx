7bd499508668ce563cc20da9b6f83c5d0fcec3e0
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-11-22 17:20:43 -0300

    chore(protectedRoute): implement the new route and make changes

--- src/components/Route/protectedRoute.tsx
+++ src/components/Route/protectedRoute.tsx
@@ -5,54 +5,55 @@ import { enumUpdate } from "../../../store/actions/enums";
 import useCRUD from "../hooks/useCRUD";
 
 const ProtectedRoute = ({ router, children }: any) => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
   //Identify authenticated user
   const { user, enums } = useSelector((state: any) => state);
   const isAuthenticated = !!user.id;
 
-  const { handleGet } = useCRUD({ model: 'public', pathOptions: '/enums'});
-  
+  const { handleGet } = useCRUD({ model: "public", pathOptions: "/enums" });
+
   useEffect(() => {
-    if(!Object.keys(enums).length) {
-        handleGet()
-              .then(({ data, error }) => {
-                if(error) return;
-                dispatch(enumUpdate(data));
-              });
-    };
-  },[]);
+    if (!Object.keys(enums || {}).length) {
+      handleGet().then(({ data, error }) => {
+        if (error) return;
+        dispatch(enumUpdate(data));
+      });
+    }
+  }, []);
 
   let protectedRoutes = {
-    student: [
-      appRoutes.logout
-    ],
+    student: [appRoutes.logout, appRoutes.classes],
     teacher: [
       appRoutes.home,
       appRoutes.logout,
-      appRoutes.registerClass
+      appRoutes.registerClass,
+      appRoutes.classes,
     ],
     admin: [
       appRoutes.home,
       appRoutes.logout,
-      appRoutes.registerTeacher
-    ]
+      appRoutes.registerTeacher,
+      appRoutes.classes,
+    ],
+  };
+
+  if (!isAuthenticated && router.pathname !== appRoutes.login) {
+    router.push(appRoutes.login);
+    return null;
   }
 
-     if (!isAuthenticated && router.pathname !== appRoutes.login) {
-       router.push(appRoutes.login);
-       return null;
-     }
+  if (
+    isAuthenticated &&
+    !protectedRoutes[user?.userType].find((url) => url === router.pathname)
+  ) {
+    router.push(protectedRoutes[user?.userType][0]);
+    return null;
+  }
 
-     if(isAuthenticated && !protectedRoutes[user?.userType].find(url => url === router.pathname)) {
-      router.push(protectedRoutes[user?.userType][0]);
-      return null;
-     }
-
-     if (isAuthenticated && router.pathname === appRoutes.login) {
-        router.push(appRoutes.home);
-        return null;
-      }
-
+  if (isAuthenticated && router.pathname === appRoutes.login) {
+    router.push(appRoutes.home);
+    return null;
+  }
 
   return children;
 };
