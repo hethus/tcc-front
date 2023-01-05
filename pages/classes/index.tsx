260059a29e79e2e0082a9f87e0123fa8775f0a39
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2023-01-05 16:16:16 -0300

    chore(Classes): remove interface and states

--- pages/classes/index.tsx
+++ pages/classes/index.tsx
@@ -8,24 +8,11 @@ import { FilterButton } from "../../src/components/filterButton";
 import ClassesTable from "../../src/components/tables/classesTable";
 import DeleteClassModal from "../../src/components/modals/deleteClass";
 
-interface Source {
-  id: string;
-  name: string;
-  students: number;
-  semester: string;
-  discipline: string;
-  more?: any;
-}
-
 const Classes: NextPage = () => {
   const { enums } = useSelector((state: any) => state);
   const hasEnums = Object.keys(enums).length;
 
-  const [deleteId, setDeleteId] = useState('');
   const [openModal, setOpenModal] = useState(false);
-
-  const { Search } = Input;
-
 
   return hasEnums ? (
     <div className={styles.container}>
