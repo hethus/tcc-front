491010b5cc7eebf6ffd0c1873fb99ea696527174
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2023-01-11 16:20:25 -0300

    feat(Classes): add BackPage component and a state to take a id in table for delete

--- pages/classes/index.tsx
+++ pages/classes/index.tsx
@@ -7,18 +7,22 @@ import { Header } from "../../src/components/header";
 import { FilterButton } from "../../src/components/filterButton";
 import ClassesTable from "../../src/components/tables/classesTable";
 import DeleteClassModal from "../../src/components/modals/deleteClass";
+import BackPage from "../../src/components/backPages";
 
 const Classes: NextPage = () => {
   const { enums } = useSelector((state: any) => state);
   const hasEnums = Object.keys(enums).length;
 
   const [openModal, setOpenModal] = useState(false);
+  const [dataIdParam, setDataIdParam] = useState('');
 
   return hasEnums ? (
     <div className={styles.container}>
       <Header />
 
       <div className={styles.body}>
+        <BackPage />
+
         <div className={styles.content}>
           <h1 className={styles.title}>Turmas</h1>
 
@@ -26,11 +30,11 @@ const Classes: NextPage = () => {
         </div>
 
         <div className={styles.ContainerTable}>
-          <ClassesTable setOpenModal={setOpenModal} />
+          <ClassesTable setOpenModal={setOpenModal} setDataIdParam={setDataIdParam} />
         </div>
 
-        {openModal ? (
-          <DeleteClassModal openModal={openModal} setOpenModal={setOpenModal} />
+        {openModal && dataIdParam !== undefined ? (
+          <DeleteClassModal openModal={openModal} setOpenModal={setOpenModal} dataIdParam={dataIdParam} />
         ) : (
           ""
         )}
